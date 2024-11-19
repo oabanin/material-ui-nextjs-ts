7b8d79199bf354f902809372a736ee92d6bca657
oleh <oleh.abanin@yolo.com>
2024-11-19 16:24:36 +0200

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/material-ui-nextjs-ts/' : '',
+    basePath: isProd ? '/material-ui-nextjs-ts' : '',
+    output: 'export'
+};
 
 export default nextConfig;
+
